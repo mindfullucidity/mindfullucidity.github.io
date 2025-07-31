d2bed8790c1ec52644ee4d0fcc84075e5c6a7c96
Will Meyburgh <annekinmeyburgh@gmail.com>
2025-07-31 18:15:29 +0200

    gh pages

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -2,13 +2,13 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
   const supabase = useSupabaseClient()
   const { data: { session } } = await supabase.auth.getSession()
 
-  // If user is not logged in and tries to access a protected page
-  if (!session && to.path !== '/login' && to.path !== '/register') {
+  const unprotectedRoutes = ['/', '/login', '/register']
+
+  if (!session && !unprotectedRoutes.includes(to.path)) {
     return navigateTo('/login')
   }
 
-  // If user is logged in and tries to access login or register page
   if (session && (to.path === '/login' || to.path === '/register')) {
-    return navigateTo('/')
+    return navigateTo('/home')
   }
 })
