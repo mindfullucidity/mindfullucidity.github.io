83649da989ab851b02e2a5445f93f788c587a0a8
Will Meyburgh <annekinmeyburgh@gmail.com>
2025-07-31 11:33:44 +0200

    journal toolbar init

--- composables/useJournal.ts
+++ composables/useJournal.ts
@@ -5,9 +5,9 @@ const selectedEntry = ref(null);
 
 const loadEntries = () => {
   entries.value = [
-    { id: 1, title: 'My First Dream', description: 'I flew over a city of clouds...', date: '2025-07-31', content: 'The clouds were pink and orange, and the city below was made of crystal. I could feel the wind on my face as I soared through the air. It was an incredible feeling of freedom.' },
-    { id: 2, title: 'Lucid Adventure', description: 'Became aware I was dreaming and...', date: '2025-07-30', content: 'I realized I was dreaming when I saw a clock with hands moving backwards. I decided to test my abilities and tried to fly. It took a few tries, but I eventually lifted off the ground and flew towards the moon.' },
-    { id: 3, title: 'Strange Encounter', description: 'Met a talking squirrel who gave me...', date: '2025-07-29', content: 'The squirrel had a tiny hat and a monocle. He offered me a glowing acorn and told me it would grant me one wish. I wished for the ability to lucid dream whenever I wanted.' },
+    { id: 1, title: 'My First Dream', date: '2025-07-31', content: 'The clouds were pink and orange, and the city below was made of crystal. I could feel the wind on my face as I soared through the air. It was an incredible feeling of freedom.' },
+    { id: 2, title: 'Lucid Adventure', date: '2025-07-30', content: 'I realized I was dreaming when I saw a clock with hands moving backwards. I decided to test my abilities and tried to fly. It took a few tries, but I eventually lifted off the ground and flew towards the moon.' },
+    { id: 3, title: 'Strange Encounter', date: '2025-07-29', content: 'The squirrel had a tiny hat and a monocle. He offered me a glowing acorn and told me it would grant me one wish. I wished for the ability to lucid dream whenever I wanted.' },
   ];
 };
 
